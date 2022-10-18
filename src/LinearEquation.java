361b4a1bf1b4a4f0be438718b812d1ce15ceb61c
Simon Huang <simonh8@nycstudents.net>
2022-10-18 09:30:39 -0400

    a

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class LinearEquation {
+public class LinearEquation {
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+
+    public LinearEquation(int x1, int y1, int x2, int y2){
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+    public double distance(){
+        return Math.sqrt(Math.pow((x2-x1)+(y2-y1),2));
+    }
+    public double slope(){
+        return (y2-y1)/(x2-x1);
+    }
+    public double yIntercept(){
+        double y;
+        double m;
+        double b;
+    }
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class LinearEquationRunner {
+public class LinearEquationRunner {
+    public static void main(String[] args){
+
+    }
 }
